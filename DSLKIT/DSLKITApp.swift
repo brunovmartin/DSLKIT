aafc5bda085f3d179fd5def2e986b5e56480cb82
Bruno Verçosa <bruno.vmartins@hotmail.com>
2025-04-26 05:50:42 -0300

    Initial Commit

---
+++ DSLKIT/DSLKITApp.swift
@@ -0,0 +1,17 @@
+//
+//  DSLKITApp.swift
+//  DSLKIT
+//
+//  Created by Pixel Logic Apps on 26/04/25.
+//
+
+import SwiftUI
+
+@main
+struct DSLKITApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
